c0f92a01bf5d734e6b4ad623b66281e7f07e8d51
aidenwong812 <wongaiden812@gmail.com>
2024-01-19 10:08:09 +1000

    add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { IBM_Plex_Mono, Inter } from 'next/font/google'
 import './globals.css'
 
 const fontSans = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+const fontMono = IBM_Plex_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+  weight: ["100", "200", "300", "400", "500", "600", "700"]
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={["font-mono", fontSans.variable].join(" ")}>
+    <html lang="en" className={["font-mono", fontSans.variable, fontMono.variable].join(" ")}>
       <body className="bg-white flex flex-col min-h-screen">{children}</body>
     </html>
   )
